d133e4d961fc008fc107e3a13eceaa2ad0364a96
Samuel Ko <koshunyin@gmail.com>
2023-06-03 11:26:07 +1000

    refactor e2e test

--- e2e/tests/repository/clone.test.ts
+++ e2e/tests/repository/clone.test.ts
@@ -1,53 +1,54 @@
 import { test, expect } from "@playwright/test";
-import { tmpdir } from "os";
 import path from "path";
+import { tmpdir } from "os";
 import { randomUUID } from "crypto";
 
 test("open and close dialog", async ({ page }) => {
-  await page.goto("http://localhost:34115/");
-
-  const button = page.getByRole("button", { name: "Clone" });
-  await button.click();
-
-  const dialog = page.getByRole("dialog");
-  await expect(dialog).toBeVisible();
+  const dialog = await openDialog(page);
 
   const closeIcon = dialog.getByLabel("Close");
   await closeIcon.click();
+  
   await expect(dialog).not.toBeVisible();
 });
 
 test("clone successfully", async ({ page }) => {
-  await page.goto("http://localhost:34115/");
-  await page.getByRole("button", { name: "Clone" }).click();
-  const dialog = page.getByRole("dialog");
+  const dialog = await openDialog(page);
 
   await dialog
     .getByLabel("Repository URL")
     .fill("https://github.com/go-git/go-git.git");
   await dialog.getByLabel("Folder Path").fill(getTempDir());
-  await page.getByRole("button", { name: "Clone" }).click();
+  await dialog.getByRole("button", { name: "Clone" }).click();
 
-  await expect(page.getByText("Cloned successfully")).toBeVisible();
+  await expect(dialog.getByText("Cloned successfully")).toBeVisible();
 });
 
 test("display error message", async ({ page }) => {
-  await page.goto("http://localhost:34115/");
-  await page.getByRole("button", { name: "Clone" }).click();
-  const dialog = page.getByRole("dialog");
+  const dialog = await openDialog(page);
 
   await dialog
     .getByLabel("Repository URL")
     .fill("https://github.com/samuelko123/no-such-repo.git");
   await dialog.getByLabel("Folder Path").fill(getTempDir());
-  await page.getByRole("button", { name: "Clone" }).click();
+  await dialog.getByRole("button", { name: "Clone" }).click();
 
   await expect(
-    page.getByText(
+    dialog.getByText(
       "Repository 'https://github.com/samuelko123/no-such-repo.git/' not found"
     )
   ).toBeVisible();
 });
+
+async function openDialog(page) {
+  await page.goto("http://localhost:34115/");
+  const button = page.getByRole("button", { name: "Clone" });
+  await button.click();
+
+  const dialog = page.getByRole("dialog");
+  await expect(dialog).toBeVisible();
+  return dialog;
+}
 
 function getTempDir() {
   return path.join(tmpdir(), randomUUID());
